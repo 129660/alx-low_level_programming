297edc9d82821e8012f4fb07398cbcca38c1b882
129660 <ashget1234@gmail.com>
2022-09-08 14:03:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- * *main - Printing
- * *
- * *Return: Always 0 (Successful)
- * */
+ * main- Printing
+ *
+ * Return: Always 0 (Successful)
+ */
 int main(void)
 {
 	   put("\"Programming is like building a multilingual puzzle");
-	      return (0);
+	   return (0);
 }
 
