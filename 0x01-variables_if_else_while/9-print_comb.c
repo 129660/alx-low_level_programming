af98c719d53caab27bf5beb7fee39da3d43e84e7
129660 <ashget1234@gmail.com>
2022-09-09 13:16:37 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,15 @@
 int main(void)
 {
 	int c;
-
-	for (c = 48 ; c <= 57 ; c++)
-	{	putchar(c);
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
 		if (c != 57)
 		{
 			putchar(',');
-                        putchar(' ');
+			putchar(' ');
 		}
 	}
-        putchar('\n');
-        return (0);
-}	
-
+	putchar('\n');
+	return (0);
+}
