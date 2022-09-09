8596742919675a37a2caa7dba1abadd903d87138
129660 <ashget1234@gmail.com>
2022-09-09 09:14:17 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,8 +4,8 @@
 /**
  * main - print if the number is positive, zero or negative
  *
- * Description using: the main function
- * this programm prints "programming is positive, zero or negative 
+ * Description: using the main function
+ * this programm prints "programming is positive, zero or negative" 
  * Return 0
  */
 int main(void)
@@ -28,6 +28,7 @@ else if (l == 0)
 else 
 { 
 	printf("last digit of %d is %d and is less than 6 and not 0\n",n, l);
+}
 return (0);
 }
 
