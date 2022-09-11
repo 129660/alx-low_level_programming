d86e9effa81677a7fd72fa414d4688534a2687f2
129660 <ashget1234@gmail.com>
2022-09-11 15:09:13 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,14 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 1; j <= 9; j+++)
+		for (j = 1; j <= 9; j++)
 		{
 			if (j < 1)
 			{
 
-				putchar(i + '0');
-				putchar(j + '0');
-				if(i ! =8)
+				putchar (i + '0');
+				putchar (j + '0');
+				if (i! =8)
 				{
 
 					putchar(',');
