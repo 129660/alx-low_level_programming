5573ae5a61c0b87368b69106217ff5b769a7d784
129660 <ashget1234@gmail.com>
2022-09-11 15:01:11 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio>
+/**
+ * main-  print single digit number starting 0
+ *
+ * Return: returns at zero at the end
+ * */
+int main(void)
+{
+
+	int i,j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 1; j <= 9; j+++)
+		{
+			if (j < 1)
+			{
+
+				putchar(i + '0');
+				putchar(j + '0');
+				if(i ! =8)
+				{
+
+					putchar(',');
+					putchar(' ');
+
+				}
+
+			}
+
+		}
+
+	}
+
+	putchar('\n');
+	return (0);
+}
