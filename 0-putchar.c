b1bff3d308744f4ce355ebaef173ceb68820f11a
129660 <ashget1234@gmail.com>
2022-09-13 09:58:40 -0700

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main()
+ * 
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	 int i;
+
+	 putchar ('X');
+	 putchar (' ');
+	 for (i=1; i<=10; i++)
+	 
+	 {
+           putchar ('!');
+
+	 }
+
+	 putchar ('\n')
+	 return 0;
+        
+}
