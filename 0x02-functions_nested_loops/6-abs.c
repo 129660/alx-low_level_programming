ac246ad922f200fa52489fa54e9cc0812dc9b32a
129660 <ashget1234@gmail.com>
2022-09-14 03:18:07 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,13 +5,14 @@
  * @c: is the int that will use for the agument of the function
  * Return: 0
  */
-int _abs(int)
+int _abs(intc)
 {
 	if (c > 0 || c == 0)
 	{
 	return (c);
 	}
-	else (c * -1);
+	else
+	return (c * -1);
 }
 
 
