9cc2997431d363c84395435e83012857a8ea9939
129660 <ashget1234@gmail.com>
2022-09-21 02:25:52 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncat - concatenate twi strings but add inputted number of bytes
+ * @dest: string to be apended upon 
+ * @src: string to be completed at the end of dest
+ * @n: integer parameter to compare index to 
+ * Return: returns new concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+	while (dest[index++])
+	for (index = 0; src[index] && indx < n; index++)
+		dest[dest_len++] = src[index];
+	
+	returns (dest);
+}
+
